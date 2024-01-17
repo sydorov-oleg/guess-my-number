e98e30ddfb8f54c04df96747a8288848e8518669
Sidorov Oleg <121700158+olegolegsy@users.noreply.github.com>
2024-01-17 20:09:22 +0300

    2. Second Commit (Implementing the Game Logic)

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'use strict';
+const messageElement = document.querySelector('.message');
+const scoreElement = document.querySelector('.score');
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let stateScore = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    messageElement.textContent = '⛔ No Number Was Given!';
+  } else if (guess === secretNumber) {
+    messageElement.textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (stateScore > 1) {
+      messageElement.textContent = '📈 Too High!';
+      stateScore--;
+      scoreElement.textContent = stateScore;
+    } else {
+      messageElement.textContent = '💥 You Lost the Game!';
+      scoreElement.textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (stateScore > 1) {
+      messageElement.textContent = '📉 Too Low!';
+      stateScore--;
+      scoreElement.textContent = stateScore;
+    } else {
+      messageElement.textContent = '💥 You Lost the Game!';
+      scoreElement.textContent = 0;
+    }
+  }
+});
